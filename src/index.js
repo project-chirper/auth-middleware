6922bf4ffd4a3bc0f9865bf8ac1824be8f78e033
Matthew Lang <matthewlang019@gmail.com>
2019-04-30 18:24:18 +0100

    added fetchUser argument

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 const axios = require('axios')
 
-module.exports = ({ required }) => {
+module.exports = ({ required, fetchUser }) => {
   return async (req, res, next) => {
     let token = req.headers['x-access-token'] // Extract token
     if (token) {
       // Verify token
+      let userId
       try {
         let response = await axios({
           url: 'http://api-gateway:3001/user/auth',
@@ -12,11 +13,28 @@ module.exports = ({ required }) => {
           headers: { 'x-access-token': token },
           responseType: 'json'
         })
+        userId = response.data
+      } catch(err) {
+        if (err.response.status === 401 && required) return res.sendStatus(401) // Invalid token and it's required
+        else if (err.response.status === 401 && !required) next() // Invalid token but not required, so move on
+      }
 
-        if (response.status === 200) req.user = response.data
-      } catch(error) {
-        if (error.response.status === 401 && required) return res.sendStatus(401) // Invalid token and it's required
-        else if (error.response.status === 401 && !required) next() // Invalid token but not required, so move on
+      console.log(userId)
+
+      // Check if fetchUser is true
+      if (fetchUser) {
+        try {
+          let response = await axios({
+            url: `http://api-gateway:3001/user/${userId}`,
+            method: 'get',
+            responseType: 'json'
+          })
+          req.user = response.data
+        } catch (err) {
+          return res.sendStatus(err.response.status)
+        }
+      } else { // If not, set req.user to user ID
+        req.user = userId
       }
     } else if (required) return res.sendStatus(401) // no token and not required, move on
     next()
